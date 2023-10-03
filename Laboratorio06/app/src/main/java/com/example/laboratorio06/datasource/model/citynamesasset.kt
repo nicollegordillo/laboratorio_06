c62e3c078f01dc2477b25dd5e1893866474b6859
nicollegordillo <98134296+nicollegordillo@users.noreply.github.com>
2023-10-03 10:14:46 -0600

    app

---
+++ Laboratorio06/app/src/main/java/com/example/laboratorio06/datasource/model/citynamesasset.kt
@@ -0,0 +1,5 @@
+package com.example.laboratorio06.datasource.model
+
+data class citynamesasset(
+    val data: List<String>
+)
---
+++ Laboratorio06/app/src/main/java/com/example/laboratorio06/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.laboratorio06.util
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://api.teleport.org/api/urban_areas/"
+    }
+}

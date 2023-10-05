76ae0f003caebf647aa173a1e37ce3b673b79e1d
nicollegordillo <98134296+nicollegordillo@users.noreply.github.com>
2023-10-05 15:13:45 -0600

    update

--- Laboratorio06/app/src/main/java/com/example/laboratorio06/datasource/api/RetrofitInstance.kt
+++ Laboratorio06/app/src/main/java/com/example/laboratorio06/datasource/api/RetrofitInstance.kt
@@ -1,13 +1,13 @@
 package com.example.laboratorio06.datasource.api
-
+/*
 import com.example.laboratorio06.util.Constants.Companion.BASE_URL
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory*/
 
 class RetrofitInstance {
-
+/*
     companion object {
         private val retrofit by lazy {
             val logging = HttpLoggingInterceptor()
@@ -26,6 +26,6 @@ class RetrofitInstance {
         val api by lazy {
             retrofit.create(citiesapi::class.java)
         }
-    }
+    }*/
 
 }
--- Laboratorio06/app/src/main/java/com/example/laboratorio06/datasource/api/citiesapi.kt
+++ Laboratorio06/app/src/main/java/com/example/laboratorio06/datasource/api/citiesapi.kt
@@ -1,16 +1,16 @@
 package com.example.laboratorio06.datasource.api
-
+/*
 import com.example.laboratorio06.datasource.model.*
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
-import retrofit2.http.Query
+import retrofit2.http.Query*/
 
 interface citiesapi {
-
+/*
     @GET("Accept: application/vnd.teleport.v1+json")
     fun getCurrencyHistory(
         
         @Query("name") name: String
-    ): Call<citynamesasset>
+    ): Call<citynamesasset>*/
 }
